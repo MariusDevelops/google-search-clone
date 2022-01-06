802b72df30722d67d7d1daa6b0967a339d932a97
MariusDevelops <brazasmarius@gmail.com>
2022-01-06 02:02:05 +0100

    Video section

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -97,7 +97,20 @@ export const Results = () => {
       );
     // 1:14:31
     case "/videos":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap ">
+          {results?.map((video, index) => (
+            <div key={index} className="p-2">
+              <ReactPlayer
+                url={video.additional_links?.[0].href}
+                controls
+                width="355px"
+                height="200px"
+              />
+            </div>
+          ))}
+        </div>
+      );
     default:
       return "ERROR";
   }
